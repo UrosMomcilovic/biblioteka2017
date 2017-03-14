83d21c4e576bde522be190d12c0a31018a6b2d33
UrosMomcilovic <urosuki968@gmail.com>
2017-03-14 15:32:19 +0100

    Dodat interfejs BibliotekaInterfejs

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -1,5 +1,46 @@
 package biblioteka;
 
-public class Knjiga {
+import java.util.LinkedList;
 
+public class Knjiga {
+	
+	private String naslov;
+	private long isbn;
+	private String izdavac;
+	private int izdanje;
+	private LinkedList<Autor> autori;
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	public LinkedList<Autor> getAutori() {
+		return autori;
+	}
+	public void setAutori(LinkedList<Autor> autori) {
+		this.autori = autori;
+	}
+	
+	
+	
 }
